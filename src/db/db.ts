ca4ce4eb28fc033efeb3ba7f6e229b05c52fff70
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2023-09-02 12:21:27 +0530

    feat: adds mongo user model and mongo handler
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

---
+++ src/db/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("MongoDB Connect ho gaya bro");
+    });
+    connection.on("error", (error) => {
+      console.log("MongoDB Connect nahi ho paya bro");
+      console.log(error);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something goes wrong!");
+    console.log(error);
+  }
+}
