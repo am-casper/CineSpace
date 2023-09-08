ca772769dc298c9bf8d3cff5283bb97654678d59
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2023-09-08 22:34:02 +0530

    fix: fixed sidebar padding, state, position,etc
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,22 +1,20 @@
 "use client";
 import React from "react";
 import "@/components/Sidebar/sidebar.css";
-import {
-  History,
-  HistoryEduRounded,
-  House,
-  Menu,
-  Subscriptions,
-} from "@mui/icons-material";
+import { History, House, Menu, Subscriptions } from "@mui/icons-material";
 import useSidebarStore from "@/global/sideBarStore";
+import { useRouter } from "next/navigation";
+
 function Sidebar() {
+  const router = useRouter();
+
   const setSidebar = useSidebarStore((state) => state.setSidebarActive);
   const sidebar = useSidebarStore((state) => state.sidebarActive);
   console.log(sidebar);
   return (
-    <div className={` ${sidebar ? "sidebaropen" : "sidebarclose"}`}>
+    <div className={`z-30 ${sidebar ? "sidebaropen" : "sidebarclose"}`}>
       <div
-        className={`menudiv`}
+        className={`z-50 menudiv`}
         onClick={() => {
           setSidebar(!sidebar);
         }}
@@ -24,15 +22,30 @@ function Sidebar() {
         <Menu className="menu" fontSize="large" />
       </div>
       <div className={`${sidebar ? "optionsopen" : "optionsclose"}`}>
-        <div className={`${sidebar ? "optionopen" : "optionclose"}`}>
+        <div
+          className={`${sidebar ? "optionopen" : "optionclose"}`}
+          onClick={() => {
+            router.push("/");
+          }}
+        >
           <House fontSize="large" />
           <p className="tags">Home</p>
         </div>
-        <div className={`${sidebar ? "optionopen" : "optionclose"}`}>
+        <div
+          onClick={() => {
+            router.push("/history");
+          }}
+          className={`${sidebar ? "optionopen" : "optionclose"}`}
+        >
           <History fontSize="large" />
           <p className="tags">History</p>
         </div>
-        <div className={`${sidebar ? "optionopen" : "optionclose"}`}>
+        <div
+          onClick={() => {
+            router.push("/subscription");
+          }}
+          className={`${sidebar ? "optionopen" : "optionclose"}`}
+        >
           <Subscriptions fontSize="large" />
           <p className="tags">Subsciptions</p>
         </div>
