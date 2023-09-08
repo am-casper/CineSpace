81d4d54cf62b245f181b4017e651d3475105652d
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2023-09-08 22:33:19 +0530

    fix: fixed navbar padding, position and responsiveness
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -4,15 +4,20 @@ import "@/components/Navbar/navbar.css";
 import { Search } from "@mui/icons-material";
 import { Avatar } from "@mui/material";
 import useSidebarStore from "@/global/sideBarStore";
+import { useRouter } from "next/navigation";
 
 function Navbar() {
+  const router = useRouter();
   const sidebar = useSidebarStore((state) => state.sidebarActive);
   const [search, setSearch] = useState("");
-  console.log(search);
-  console.log("inside navbar", sidebar);
+  const searchkaro = () => {
+    if (search) {
+      router.push(`/${search}`);
+    }
+  };
   return (
     <div
-      className={`flex flex-row justify-between w-screen px-5 py-2 bg-white items-center absolute top-0 ${
+      className={`flex flex-row justify-between w-screen px-5 py-2 bg-white items-center fixed z-10 top-0 ${
         sidebar ? "sidebaractiv" : ""
       }`}
     >
@@ -25,12 +30,15 @@ function Navbar() {
             setSearch(e.target.value);
           }}
         />
-        <div className="search">
+        <div className="search" onClick={searchkaro}>
           <Search fontSize="large" className="searchicon" />
         </div>
       </div>
       <div>
-        <Avatar src="https://media.licdn.com/dms/image/D4E03AQGI1ZJx1AywYQ/profile-displayphoto-shrink_800_800/0/1665646742212?e=1699488000&v=beta&t=Td2ujhuMGBT5UARVIpY3gbyKxmOeLF6qL7Qw7bCxhM8" />
+        <Avatar
+          className=""
+          src="https://media.licdn.com/dms/image/D4E03AQGI1ZJx1AywYQ/profile-displayphoto-shrink_800_800/0/1665646742212?e=1699488000&v=beta&t=Td2ujhuMGBT5UARVIpY3gbyKxmOeLF6qL7Qw7bCxhM8"
+        />
       </div>
     </div>
   );
