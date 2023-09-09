b2c95e51fdbdffa685a2476ad602fd15bffd5091
Aayushmaan <goforaayush@gmail.com>
2023-09-09 13:42:06 +0530

    merge from casper into kitu

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ import useSidebarStore from "@/global/sideBarStore";
 import "@/styles/main.css";
 export default function Home() {
   const {VidpublicId: vidLink} = useUploadContext()
-  const sbactive = useSidebarStore((state) => state.sidebarActive);
+  const sbactive = useSidebarStore((state:any) => state.sidebarActive);
   return (
     <>
       <Navbar />
