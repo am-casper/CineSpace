f9490424245f4df0f177429a56f80d62baababb3
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2023-09-10 17:44:28 +0530

    chore: duplicate code
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- frontend/src/db/db.ts
+++ frontend/src/db/db.ts
@@ -17,22 +17,4 @@ export async function connect() {
     console.log(error);
   }
 }
-import mongoose from "mongoose";
 
-export async function connect() {
-  try {
-    mongoose.connect(process.env.MONGO_URL!);
-    const connection = mongoose.connection;
-    connection.on("connected", () => {
-      console.log("MongoDB Connect ho gaya bro");
-    });
-    connection.on("error", (error) => {
-      console.log("MongoDB Connect nahi ho paya bro");
-      console.log(error);
-      process.exit();
-    });
-  } catch (error) {
-    console.log("Something goes wrong!");
-    console.log(error);
-  }
-}
